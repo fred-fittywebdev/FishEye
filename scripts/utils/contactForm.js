fee5f8352b0691a41e00640889449e898bd6bdb3
fred-fittywebdev <contact@fittywebdev.fr>
2023-08-04 10:46:29 +0200

    ♻️ Ajout de la validation au niveau du form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,13 +1,37 @@
 const modalEl = document.getElementById("contact_modal");
 const closeContactEl = document.getElementById("close-contact");
-console.log(closeContactEl);
 const openContactEl = document.getElementById("contact_button");
+const formEl = document.querySelector("form");
+console.log(formEl.elements);
+
+function checkValidity() {
+	for (let element of formEl.elements) {
+		if (element.validity.tooShort) {
+			element.setCustomValidity("Veuillez entrer 4 caractères minimums.");
+		} else if (element.validity.typeMismatch) {
+			element.setCustomValidity(
+				"Veuillez entrer une adresse mail valide."
+			);
+		} else if (element.validity.valueMissing) {
+			element.setCustomValidity("Ce champ ne peut être vide");
+		} else {
+			element.setCustomValidity("");
+		}
+	}
+}
+
+checkValidity();
+
+for (let element of formEl.elements) {
+	element.addEventListener("input", checkValidity);
+}
 
 function displayModal() {
 	modalEl.style.display = "block";
 }
 
 export function closeModal() {
+	formEl.reset();
 	modalEl.style.display = "none";
 }
 
